0ea091c9654e084bade9c799d8a16254ebfc2ea4
Sigrun Olafsdottir <solafsdottir@gmail.com>
2020-12-03 20:21:19 +0100

    AoC day 3, lambda added

--- src/AoC/Dec3/Dec3b.kt
+++ src/AoC/Dec3/Dec3b.kt
@@ -5,28 +5,27 @@ import AoC.makeStringList
 
 fun main(){
 
+    data class Pair (val deltaX : Int, val deltaY: Int)
+
     val input = makeStringList ("src/AoC/Dec3/input.txt")
 
-    fun countTrees(deltaX : Int, deltaY : Int) : Int {
+    val slopes : List<Pair> = listOf(Pair(1,1), Pair(3,1),
+        Pair(5,1), Pair(7,1), Pair(1,2))
+
+    fun countTrees(coords : Pair) : Int {
         var counter : Int = 0
         var x : Int = 0
         val width : Int = input.get(0).length
         val rowNum : Int = input.size -1
-        for (y in 0..rowNum-deltaY step deltaY) {     //iter row
-            x = (x + deltaX) % width
-            if (input.get(y+deltaY).get(x) == '#') counter++
+        for (y in 0..rowNum-coords.deltaY step coords.deltaY) {     //iter row
+            x = (x + coords.deltaX) % width
+            if (input.get(y+coords.deltaY).get(x) == '#') counter++
         }
         return counter
     }
 
-    val a = countTrees(1, 1)
-    val b = countTrees(3, 1)
-    val c = countTrees(5, 1)
-    val d = countTrees(7, 1)
-    val e = countTrees(1, 2)
 
-    println("a: $a, b: $b, c: $c, d: $d, e: $e")
-    val prod : Long = a.toLong()*b.toLong()*c.toLong()*d.toLong()*e.toLong()
-    println(prod)
+    println(slopes.map{countTrees(it).toLong()}.reduce { acc, e -> e*acc })
+
 
 }
