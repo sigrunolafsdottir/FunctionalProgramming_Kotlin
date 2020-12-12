0ca3a71af4ef393cae4837279948e117bee0cb42
Sigrun Olafsdottir <solafsdottir@gmail.com>
2020-12-12 21:57:38 +0100

    AoC 9 cleanup

--- src/AoC/Dec9/Dec9b.kt
+++ src/AoC/Dec9/Dec9b.kt
@@ -7,18 +7,18 @@ fun main(){
     val input: List<Long> = File("src/AoC/Dec9/input.txt").readLines().map{it.toLong()}
     val target: Long = 26134589  //from uppg9A
 
+
     fun findRange(list : List<Long>) : List<Long>{
-        var counter : Int = 0
         var sum : Long = 0
-        for(i in 0..list.size){
+        for(i in 0..list.size-1){
             sum += list.get(i)
-            counter ++
-            if (sum == target) return list.subList(0, i+1)
-            else if (sum > target) return listOf()
+            when {
+                sum == target -> return list.subList(0, i + 1)
+                sum > target -> return listOf()
+            }
         }
         return listOf()
     }
-
 
     fun isItAddingUp() : Long{
         for (i in 0..input.size-1){
