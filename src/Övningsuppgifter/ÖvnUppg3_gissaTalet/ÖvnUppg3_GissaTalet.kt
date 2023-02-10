4eb5b214af5d581853c87fc9f905908c9e67c2d0
sigrunolafsdottir <solafsdottir@gmail.com>
2023-02-10 10:46:23 +0100

    Added some examples

--- src/Övningsuppgifter/ÖvnUppg3_gissaTalet/ÖvnUppg3_GissaTalet.kt
+++ src/Övningsuppgifter/ÖvnUppg3_gissaTalet/ÖvnUppg3_GissaTalet.kt
@@ -5,7 +5,10 @@ fun main (){
     var randomNumber = (1..10).random()
     println("Gissa en siffra mellan 1 och 10")
     while(true) {
-        val guessed = readLine()!!.toInt()
+
+       // val guessed = readLine()!!.toInt()    //  String?
+        val guessed = readLine()?.toInt() ?: 5
+
         if (randomNumber == guessed) {
             println("Det var rätt.")
             randomNumber = (1..10).random()
